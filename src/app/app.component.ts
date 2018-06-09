5782710efba58b4682f4f02653c6b6214143a7c1
Pegah Eizadkhah <Peizadkhah@nexient.com>
2018-06-09 18:40:08 -0400

    Share services and data.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { MailService } from './mail.service';
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<div>
+    <app-simple-form></app-simple-form>
+    {{mail.message}}
+  </div>`
 })
 export class AppComponent {
   title = 'Let\'s get started!';
+
+  constructor(private mail: MailService) {}
 }
